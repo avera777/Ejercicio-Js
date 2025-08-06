0246a664e2506a16a739fa62b4921062cfbf0952
alejandro <abel152_7@hotmail.com>
2025-08-06 12:16:33 -0500

    feat: ejercicio variables

---
+++ clase-variables.js
@@ -0,0 +1,31 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy);
+
+//Ejemplo var
+var nombre = 'Juan';
+console.log(nombre);//nombre
+
+
+//Ejemplo let
+let edad = 16;
+console.log(edad);//edad
+
+
+//Ejemplo const
+const ciudad = 'Ciudad de México';
+console.log (ciudad);//Ciudad de México
+
+
+// Paso 1: Creamos una variable con la edad actual
+let edadActual = 20;
+
+// Paso 2: Creamos otra variable con el número de años a futuro
+let añosEnElFuturo = 5;
+
+// Paso 3: Calculamos la edad futura
+let edadFutura = edadActual + añosEnElFuturo;
+
+// Paso 4: Mostramos los datos en consola
+console.log("Tu edad actual es:", edadActual);
+console.log("En", añosEnElFuturo, "años tendrás:", edadFutura);
+
