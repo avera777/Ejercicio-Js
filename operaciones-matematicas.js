efeb2a277120b5b9ceb44e7146bccf6e86334ddd
alejandro <abel152_7@hotmail.com>
2025-08-11 11:27:47 -0500

    feat: operaciones matematicas

---
+++ operaciones-matematicas.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 5;
+
+console.log("Suma:", a + b);        // 15
+console.log("Resta:", a - b);       // 5
+console.log("Multiplicación:", a * b); // 50
+console.log("División:", a / b);    // 2
+console.log("Módulo (resto):", a % b); // 0
