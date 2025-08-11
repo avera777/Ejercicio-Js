6f2865b0a1a50cf7605e8bf5f755e5cb66d74426
alejandro <abel152_7@hotmail.com>
2025-08-11 11:57:30 -0500

    feat: area perímetro del círculo

---
+++ area-perímetro-del-círculo.js
@@ -0,0 +1,6 @@
+let radio = 4;
+let area = Math.PI * Math.pow(radio, 2);
+let perimetro = 2 * Math.PI * radio;
+
+console.log("Área:", area.toFixed(2));       // 50.27
+console.log("Perímetro:", perimetro.toFixed(2)); // 25.13
