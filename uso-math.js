ceaf7c961ee7eb41cf06ab76842dc20b498929ed
alejandro <abel152_7@hotmail.com>
2025-08-11 11:30:16 -0500

    feat: uso math

---
+++ uso-math.js
@@ -0,0 +1,7 @@
+let num = 7.56;
+
+console.log("Redondear:", Math.round(num));     // 8
+console.log("Redondear hacia abajo:", Math.floor(num)); // 7
+console.log("Redondear hacia arriba:", Math.ceil(num)); // 8
+console.log("Potencia:", Math.pow(2, 3));       // 8
+console.log("Raíz cuadrada:", Math.sqrt(25));   // 5
