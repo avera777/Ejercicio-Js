20ca7f0d37072f7105de97796ec5a06da5cef72c
alejandro <abel152_7@hotmail.com>
2025-08-11 11:21:37 -0500

    feat: concatenacion substrings

---
+++ concatenacion-substrings.js
@@ -0,0 +1,9 @@
+let string1 = 'Hola, mundo'
+let string2 = "JavaScript es genial"
+let string3 = `${string1} feliz :)`
+let string4 = string1 + string2
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
